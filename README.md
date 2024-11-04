dabe088869b77d8f1a325e1249b572c1b95eac2c
pro-nav <pranavsd152@gmail.com>
2024-11-04 19:08:30 +0530

    removed lodash dependency, improved error handling, shows all available meanings for a word.

--- README.md
+++ README.md
@@ -2,10 +2,11 @@
 Custom JS Script for Kindle like dictionary for [LNReader](https://github.com/LNReader/lnreader).
 
 <p align="center">
-    <img src="images/screenshot_v2.png" width="200" alt="Screenshot" />
-    <img src="images/screenshot_v1.9.png" width="200" alt="Screenshot" />
+    <img src="images/screenshot_v2-1.png" width="200" alt="Screenshot Light" />
+    <img src="images/screenshot_v2-2.png" width="200" alt="Screenshot Dark" />
+    <img src="images/screenshot_v2-3.png" width="200" alt="Screenshot Black" />
 </p>
 
 Select a word and get its meaning right in the book. This script uses free dictionary API by [https://dictionaryapi.dev](https://dictionaryapi.dev/)
 
-To use this script simply copy the script from `dist/compiled.js` and paste it at the end of your custom js. Custom themes might not work in older versions of app.
+To use this script simply everything from `main.js` and paste it at the end of your custom JS. Custom themes might not work in older versions of app.
--- main.js
+++ main.js
@@ -1,95 +1,129 @@
+// Dictionary Script starts here --------------------------------------------------------------------------
+(() => {
+    function createView() {
+        const view = document.createElement('div');
+        const styles = getComputedStyle(document.body)
 
-import debounce from 'lodash.debounce'
-
-function createView() {
-    const view = document.createElement('div');
-    view.setAttribute("id", "scnid")
-    const styles = getComputedStyle(document.body)
-
-    view.style.minWidth = '300px';
-    view.style.width = '90vw';
-    view.style.maxHeight = '280px';
-    view.style.height = 'auto';
-    view.style.position = 'fixed';
-    view.style.bottom = '50px';
-    view.style.left = '50%';
-    view.style.display = 'none';
-    view.style.transform = 'translateX(-50%)';
-    view.style.fontFamily = 'inherit';
-    view.style.fontSize = 'inherit';
-    view.style.borderRadius = '3px';
-    view.style.overflowY = 'auto';
+        view.style.minWidth = '300px';
+        view.style.width = '90vw';
+        view.style.maxHeight = '280px';
+        view.style.height = 'auto';
+        view.style.position = 'fixed';
+        view.style.bottom = '50px';
+        view.style.left = '50%';
+        view.style.display = 'none';
+        view.style.transform = 'translateX(-50%)';
+        view.style.fontFamily = 'inherit';
+        view.style.fontSize = 'inherit';
+        view.style.borderRadius = '5px';
+        view.style.overflowY = 'auto';
 
 
-    view.style.background = styles.getPropertyValue("--readerSettings-theme") || '#292832'
-    view.style.color = styles.getPropertyValue("--readerSettings-textColor") || '#CCCCCC'
-    view.style.padding = styles.getPropertyValue("--readerSettings-padding") || '4px'
-    view.style.marginBottom = styles.getPropertyValue("--readerSettings-padding") || '4px'
-    view.style.border = `3px double ${styles.getPropertyValue("--readerSettings-textColor") || '#CCCCCC'}`;
+        view.style.background = styles.getPropertyValue("--readerSettings-theme") || '#292832'
+        view.style.color = styles.getPropertyValue("--readerSettings-textColor") || '#CCCCCC'
+        view.style.padding = styles.getPropertyValue("--readerSettings-padding") || '4px'
+        view.style.marginBottom = styles.getPropertyValue("--readerSettings-padding") || '4px'
+        view.style.border = `3px double ${styles.getPropertyValue("--readerSettings-textColor") || '#CCCCCC'}`;
 
-    return view;
-}
-
-let d = createView()
-const p = (e) => {
-    const l = window.getSelection().toString().trim()
-    if (l && !/\s/.test(l)) {
-        getDefinition(l)
-    } else {
-        clearDiv()
+        return view;
     }
-}
 
-document.body.appendChild(d)
-document.addEventListener('selectionchange', debounce(p, 700))
-
-let clearDiv = () => {
-    d.textContent = '';
-    d.style.display = 'none';
-}
-
-let setErrorTimeout = null;
-
-function setData(data) {
-    if (!data || data?.error) {
-        d.textContent = data?.error ? 
-        `
-        <h3>${data.word}</h3>
-        <hr>
-        <p>${data.error}</p>
-        `
-        : 'No data found'
-        setErrorTimeout = setTimeout(clearDiv, 3000);
-    } else {
-        clearTimeout(setErrorTimeout);
-        d.style.display = 'block';
-        d.innerHTML = 
-        `<h3>${data.word}</h3>` + (data?.phonetic ? `<p>${data.phonetic}</p>` : '') +
-        `<hr>` +
-        (data?.def ? `<p>${data.def}</p>` : 'No defination found') +
-        (data?.eg ? `<p>Eg: ${data.eg}</p>` : '')
+    const debounce = (callback, wait) => {
+        let timeoutId = null;
+        return (...args) => {
+            window.clearTimeout(timeoutId);
+            timeoutId = window.setTimeout(() => {
+                callback(...args);
+            }, wait);
+        };
     }
-}
 
-
-
-async function getDefinition(word) {
-    let url =
-        "https://api.dictionaryapi.dev/api/v2/entries/en/" + word;
-
-    try {
-        const response = await fetch(url);
-        const fetchedData = await response.json();
-        if (response.status === 200) {
-            let rWord = fetchedData[0]?.word;
-            let phonetic = fetchedData[0]?.phonetic;
-            let def = fetchedData[0]?.meanings[0]?.definitions[0]?.definition;
-            let eg = fetchedData[0]?.meanings[0]?.definitions[0]?.example;
-            setData({ word: rWord , def, eg, phonetic })
+    const selectionFilter = (e) => {
+        const l = window.getSelection().toString().trim()
+        if (l && !/\s/.test(l)) {
+            getDefinition(l)
         } else {
-            setData( { word, error: "Word not found in the database" });
+            clearDiv()
         }
-    } catch (error) {
-        setData({ word, error })
     }
-}
+
+    let d = createView()
+    let setErrorTimeout = null;
+
+    document.body.appendChild(d)
+    document.addEventListener('selectionchange', debounce(selectionFilter, 700))
+
+    const clearDiv = () => {
+        meanings = [];
+        d.innerHTML = '';
+        d.style.display = 'none';
+    }
+
+    const setData = (data) => {
+        if (!Number.isFinite(data?.count) || data?.count === 0) {
+            d.style.display = 'block';
+            d.innerHTML =
+                (data?.word ? `<h3>${data.word}</h3> <hr>` : '') + `<p>${data.error ? data.error : 'No data found'}</p>`;
+            setErrorTimeout = setTimeout(clearDiv, 3000);
+        } else {
+            clearTimeout(setErrorTimeout);
+            d.style.display = 'block';
+            d.innerHTML = '';
+
+            data.meanings.forEach((wordData) => {
+                const wordElement = document.createElement('div');
+                wordElement.className = 'word';
+                wordElement.innerHTML =
+                    `<div style="display: flex; align-items: baseline; gap: 5px">
+    <h3>${wordData.word}</h3>
+    ${wordData?.phonetic ? `<i>${wordData.phonetic}</i>` : ''}
+</div>`;
+
+                wordData.meanings.forEach((meaning) => {
+                    const meaningElement = document.createElement('div');
+                    meaningElement.className = 'meaning';
+                    meaningElement.innerHTML = `<hr><b>${meaning.partOfSpeech}</b>`;
+
+                    meaning.definitions.forEach((definition) => {
+                        const definitionElement = document.createElement('div');
+                        definitionElement.className = 'definition';
+                        definitionElement.innerHTML = `
+                            <p>${definition?.definition}</p>
+                            ${(definition?.example || definition?.synonyms || definition?.antonyms) ? `<blockquote>
+                                ${definition?.example ? `<p>Eg: <q>${definition.example}</q></p>` : ''}
+                                ${definition?.synonyms?.length ? `<p>${definition.synonyms.join(', ')}</p>` : ''}
+                                ${definition?.antonyms?.length ? `<p>${definition.antonyms.join(', ')}</p>` : ''}
+                            </blockquote>`: ''}
+                        `;
+
+                        meaningElement.appendChild(definitionElement);
+                    });
+
+                    wordElement.appendChild(meaningElement);
+                });
+
+                d.appendChild(wordElement);
+            });
+        }
+        d.scrollTop = 0;
+    }
+
+    async function getDefinition(word) {
+        let url =
+            "https://api.dictionaryapi.dev/api/v2/entries/en/" + word;
+
+        try {
+            const response = await fetch(url);
+            const fetchedData = await response.json();
+            if (response.status === 200) {
+                const count = fetchedData.length;
+                if (count === 0)
+                    throw new Error("Word not found in the database");
+                setData({ word, meanings: fetchedData, count })
+            } else throw new Error('Error fetching data')
+        } catch (error) {
+            setData({ word, error })
+        }
+    }
+})();
+// Dictionary Script ends here --------------------------------------------------------------------------
