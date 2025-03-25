f4a44f45291d9db1e9c53c6323864e65df419920
Pranav Dudhane <pranavsd152@gmail.com>
2025-03-25 16:59:54 +0000

    Added close button

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Dictionary Script starts here --------------------------------------------------------------------------
+/////////////// Kindle Dictionary Script - Start //////////////////////////
 (() => {
     function createView() {
         const view = document.createElement('div');
@@ -28,6 +28,28 @@
         return view;
     }
 
+    const createCloseButton = (() => {
+        const btn = document.createElement("button")
+        btn.style.background = "transparent";
+        btn.style.border = "none";
+        btn.style.color = "#CCCCCC";
+        btn.style.marginTop = "10px";
+        btn.style.marginRight = "10px"
+        btn.style.cursor = "pointer";
+        btn.style.fontSize = "16px";
+        btn.onclick = () => clearDiv()
+
+        const btnDiv = document.createElement("div");
+        btn.innerHTML = "X";
+        btnDiv.style.position = "sticky";
+        btnDiv.style.top = "0px";
+        btnDiv.style.width = "100%";
+        btnDiv.style.textAlign = "right";
+        btnDiv.appendChild(btn)
+
+        return btnDiv;
+    })
+
     const debounce = (callback, wait) => {
         let timeoutId = null;
         return (...args) => {
@@ -42,8 +64,6 @@
         const l = window.getSelection().toString().trim()
         if (l && !/\s/.test(l)) {
             getDefinition(l)
-        } else {
-            clearDiv()
         }
     }
 
@@ -64,17 +84,19 @@
             d.style.display = 'block';
             d.innerHTML =
                 (data?.word ? `<h3>${data.word}</h3> <hr>` : '') + `<p>${data.error ? data.error : 'No data found'}</p>`;
+            d.appendChild(createCloseButton())
             setErrorTimeout = setTimeout(clearDiv, 3000);
         } else {
             clearTimeout(setErrorTimeout);
             d.style.display = 'block';
             d.innerHTML = '';
+            d.appendChild(createCloseButton())
 
-            data.meanings.forEach((wordData) => {
+            data.meanings.forEach((wordData, i) => {
                 const wordElement = document.createElement('div');
                 wordElement.className = 'word';
                 wordElement.innerHTML =
-                    `<div style="display: flex; align-items: baseline; gap: 5px">
+                    `<div style="display: flex; align-items: baseline; gap: 5px; ${i !== 0 ? "margin-top: 10px; padding-top: 5px; border-top: 3px double;" : "margin-top: -20px;" }">
     <h3>${wordData.word}</h3>
     ${wordData?.phonetic ? `<i>${wordData.phonetic}</i>` : ''}
 </div>`;
@@ -90,9 +112,9 @@
                         definitionElement.innerHTML = `
                             <p>${definition?.definition}</p>
                             ${(definition?.example || definition?.synonyms || definition?.antonyms) ? `<blockquote>
-                                ${definition?.example ? `<p>Eg: <q>${definition.example}</q></p>` : ''}
-                                ${definition?.synonyms?.length ? `<p>${definition.synonyms.join(', ')}</p>` : ''}
-                                ${definition?.antonyms?.length ? `<p>${definition.antonyms.join(', ')}</p>` : ''}
+                                ${definition?.example ? `<p>&nbsp;Eg: <q>${definition.example}</q></p>` : ''}
+                                ${definition?.synonyms?.length ? `<p>&nbsp;${definition.synonyms.join(', ')}</p>` : ''}
+                                ${definition?.antonyms?.length ? `<p>&nbsp;${definition.antonyms.join(', ')}</p>` : ''}
                             </blockquote>`: ''}
                         `;
 
@@ -126,4 +148,4 @@
         }
     }
 })();
-// Dictionary Script ends here --------------------------------------------------------------------------
+/////////////// Kindle Dictionary Script - End //////////////////////////
--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    entry: './main.js',
-    output: {
-        filename: './compiled.js'
-    },
-    mode: 'production',
-    target: 'web',
-};
